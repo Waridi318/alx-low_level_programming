60a3eaeefe57bac65b459ffa6ae1b360a6a51dbe
Waridi318 <rosenjeri7151@gmail.com>
2022-06-30 13:53:43 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
 /**
-* main - entry point
-* Return: 0 if exited properly, non-zero otherwise
-*/
+ * main - prints the size of various types
+ * Return: 0 if exited properly, non-zero otherwise
+ */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
-} 
+	puts("\"Programming is like building a multiligual puzzle");
+	return (0);
+}
