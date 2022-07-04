83b61174c355afe9868647badc2be7219cc8825d
Waridi318 <rosenjeri7151@gmail.com>
2022-07-04 15:40:36 -0700

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -11,15 +11,13 @@ int main(void)
 int num;
 
 for (num = 0; num <= 9; num++)
-
 putchar((num % 10) + '0');
 if (num == 9)
 continue;
-
 putchar(',');
 putchar(' ');
 
-char('\n');
+putchar('\n');
 
 return (0);
 }
