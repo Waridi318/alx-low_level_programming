8b85b042cc0aa39d0dc814d294cb964c536c0746
Waridi318 <rosenjeri7151@gmail.com>
2022-07-06 16:02:02 -0700

    101-natural.c

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,49 +1,31 @@
-#include "main.h"
+#include <stdio.h>
 
 /**
- * print_times_table - checks if is greater than 15 or less than 0
- * @n: int
+ * main - computes and prints the sum of all the multiples of 3 or 5
+ * Return: 0 Always -> (Success)
  */
 
-void print_times_table(int n)
+int main(void)
 {
-	int i, j, k;
+	unsigned long int sum3, sum5, sum;
+	int i;
 
-	if (n >= 0 && n <= 15)
+	sum3 = 0;
+	sum5 = 0;
+	sum = 0;
+
+	for (i = 0; i < 1024; ++i)
 	{
-		for (i = 0; i <= n; i++)
+		if ((i % 3) == 0)
 		{
-			for (j = 0; j <= n; j++)
-			{
-				k = j * i;
-				if (j == 0)
-				{
-					_putchar(k + '0');
-				} else if (k < 10 && j != 0)
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(k + '0');
-				} else if (k >= 10 && k < 100)
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar((k / 10) + '0');
-					_putchar((k % 10) + '0');
-				} else if (k >= 100)
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar((k / 100) + '0');
-					_putchar(((k / 10) % 10) + '0');
-					_putchar((k % 10) + '0');
-				}
-			}
-			_putchar('\n');
+			sum3 = sum3 + i;
+		} else if ((i % 5) == 0)
+		{
+			sum5 = sum5 + i;
 		}
 	}
+	sum = sum3 + sum5;
+	printf("%lu\n", sum);
+	return (0);
 }
 
