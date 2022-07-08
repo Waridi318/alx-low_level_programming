ea8534d2f6bf5a84d741cd3958b5b5a5efb76cad
Waridi318 <rosenjeri7151@gmail.com>
2022-07-08 16:01:30 -0700

    1-main.c

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -12,7 +12,7 @@ int i;
 printf("Infinite loop incoming :(\n");
 
 i = 0;
-/*
+/**
  * while (i < 10)
  * {
  * putchar(i);
@@ -22,4 +22,3 @@ printf("Infinite loop avoided! \\o\/\n");
 
 return (0);
 }
-
