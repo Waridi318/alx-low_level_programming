cf053a3540d7388bf58e49be95009f28c7d56792
Waridi318 <rosenjeri7151@gmail.com>
2022-12-10 04:03:48 -0800

    Correction

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,13 @@
 /**
  * main - Entry point
  *
- * Return: Always 0 (Success)
+ * Return: 1 (Success)
  */
 
 #include <stdio.h>
-#include <unistd.h>
+
 int main(void)
 {
-	write(2, "and that piece of art is useful\" -
-		Dora Korpar, 2015-10-19\n", 59);
+	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, \2015-10-19\n");
 	return (1);
 }
