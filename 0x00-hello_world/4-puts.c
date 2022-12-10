f0cc183616183159de58026b015955e63f82e0b8
Waridi318 <rosenjeri7151@gmail.com>
2022-12-10 01:26:03 -0800

    Task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+/**
+ * main - Code begins
+ * puts - prints text followed by new line
+ *
+ * Return: 0
+ */
+
+#include <stdio.h>
+int main(void)
+{
+	char my_str[] = "\"Programming is like building a multilingual puzzle";
+
+	puts(my_str);
+
+	return (0);
+}
