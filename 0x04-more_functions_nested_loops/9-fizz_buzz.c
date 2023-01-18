1b14e2fd9e7518f0c13ac171975adc24840d48f4
Waridi318 <rosenjeri7151@gmail.com>
2023-01-18 10:50:53 -0800

    Code correction

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -12,23 +12,26 @@ int main(void)
 
 	for (i = 1; i <= 100; i++)
 	{
-		if ((i % 5 == 0) && (i % 3 == 0))
-		{
-			printf("FizzBuzz");
-		}
-		else if ((i % 3) == 0)
-		{
-			printf("Fizz");
-		}
-		else if ((i % 5) == 0)
+		if (i == 100)
 		{
 			printf("Buzz");
 		}
+		else if ((i % 5 == 0) && (i % 3 == 0))
+		{
+			printf("FizzBuzz ");
+		}
+		else if ((i % 3) == 0)
+		{
+			printf("Fizz ");
+		}
+		else if ((i % 5) == 0)
+		{
+			printf("Buzz ");
+		}
 		else
 		{
-			printf("%d", i);
+			printf("%d ", i);
 		}
-		printf(" ");
 	}
 	printf("\n");
 	return (0);
