b5b4e8c8c9631d5be02d4777ae4a781ccaafef9f
Waridi318 <rosenjeri7151@gmail.com>
2023-01-22 18:28:27 -0800

    Function pointers quizzes

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * print_name - prints a name
+ * @name: str
+ * @f: func
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+if (f != NULL)
+f(name);
+}
