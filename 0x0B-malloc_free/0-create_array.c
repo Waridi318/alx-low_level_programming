e5b59dc437403090d393a279b4609f1a7837f11b
Waridi318 <rosenjeri7151@gmail.com>
2023-03-08 13:24:38 -0800

    Task 0 Corrections

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -13,9 +13,9 @@ char *create_array(unsigned int size, char c)
 	unsigned int i;
 
 	ptr = malloc(size);
+	if (size == 0 || ptr == NULL)
+		return (NULL);
 	for (i = 0; i <= size; i++)
 		ptr[i] = c;
-	if (size == 0 || ptr == 0)
-		return (0);
 	return (ptr);
 }
