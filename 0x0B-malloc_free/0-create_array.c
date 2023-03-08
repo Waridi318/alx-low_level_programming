fd8e0747b855380c69481e54490de7605aa8febe
Waridi318 <rosenjeri7151@gmail.com>
2023-03-08 10:55:42 -0800

    Return error

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -12,10 +12,10 @@ char *create_array(unsigned int size, char c)
 	char *ptr;
 	unsigned int i;
 
-	ptr = malloc(sizeof(char) * size);
+	ptr = (char *)malloc(sizeof(char) * size);
 	for (i = 0; i <= size; i++)
 		ptr[i] = c;
-	if (size == 0 || ptr == NULL)
-		return (NULL);
+	if (size == 0 || ptr == 0)
+		return (0);
 	return (ptr);
 }
