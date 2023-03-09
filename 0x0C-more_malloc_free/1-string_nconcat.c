9b0b4baccab4c2701741a8f96d6c585d3cc0d17e
Waridi318 <rosenjeri7151@gmail.com>
2023-03-09 11:09:16 -0800

    Task 2

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -29,8 +29,8 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		return (NULL);
 	for (i = 0; i < len1; i++)
 		ptr[i] = s1[i];
-	for (i = 0; i < n; i++)
-		ptr[len1 + i] = s2[i];
+	for (j = 0; j < n; j++)
+		ptr[len1 + j] = s2[j];
 	ptr[len1 + n] = '\0';
 	return (ptr);
 }
---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+ * _calloc - allocates memory for an array using malloc
+ * @nmemb: elements
+ * @size: bytes
+ * Return: ptr-> allocated memory
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *ptr;
+	char *str;
+	unsigned int i;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	ptr = malloc(nmemb * size);
+	if (ptr == NULL)
+		return (NULL);
+	
+	str = ptr;
+	for (i = 0; i < (size * nmemb); i++)
+		str[i] = 0;
+	return (ptr);
+}
