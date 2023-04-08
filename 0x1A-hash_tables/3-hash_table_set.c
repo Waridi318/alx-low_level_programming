7385b6835a27d48a3e55a4ad77a1b6caf5162ae4
Waridi318 <rosenjeri7151@gmail.com>
2023-04-08 17:38:04 +0300

    Task 3.7

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -34,36 +34,29 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	hash_node_t *item;
 	int index;
 
-	if (ht == NULL)
+	item = node_create(key, value);
+	index = key_index((const unsigned char *)key, ht->size);
+
+	hash_node_t *current_item = ht->array[index];
+
+	if (current_item == NULL)
 	{
-		hash_table_create(0);
+		ht->array[index] = item;
+		return (1);
 	}
 	else
 	{
-		item = node_create(key, value);
-		index = key_index((const unsigned char *)key, ht->size);
-
-		hash_node_t *current_item = ht->array[index];
-
-		if (current_item == NULL)
+		if (strcmp(current_item->key, key) == 0)
 		{
-			ht->array[index] = item;
+			strcpy(ht->array[index]->value, value);
 			return (1);
 		}
 		else
 		{
-			if (strcmp(current_item->key, key) == 0)
-			{
-				strcpy(ht->array[index]->value, value);
-				return (1);
-			}
-			else
-			{
-				ht->array[index] = item;
-				item->next = current_item;
-				current_item = item;
-				return (1);
-			}
+			ht->array[index] = item;
+			item->next = current_item;
+			current_item = item;
+			return (1);
 		}
 	}
 	return (0);
