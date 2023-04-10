ee41982418d9e6b6592a03847e24cd91cf4cdb12
Waridi318 <rosenjeri7151@gmail.com>
2023-04-10 11:18:36 +0300

    Task 5

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,31 @@
+#include "hash_tables.h"
+#include <stdio.h>
+/**
+ * hash_table_print - prints a hash table
+ * @ht: hash table
+ * Return: Nothing
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned long int i;
+	hash_node_t *index;
+
+	if (ht != NULL)
+	{
+		printf("{");
+		for (i = 0; i < ht->size; i++)
+		{
+			index = ht->array[i];
+			while (index)
+			{
+				printf("\'%s\': \'%s\'", index->key, index->value);
+				if (index->next)
+					printf(", ");
+				index = index->next;
+			}
+		}
+		printf("}\n");
+	}
+	else
+		printf("{}\n");
+}
