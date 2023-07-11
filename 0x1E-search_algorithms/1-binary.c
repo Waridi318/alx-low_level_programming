2d9b7b3f3d4706f78d89b3b17b4fc6307524e587
Waridi318 <rosenjeri7151@gmail.com>
2023-07-11 19:00:05 +0300

    Task 1 updated

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -13,7 +13,7 @@ int binary_search(int *array, size_t size, int value)
 
 	if (array == NULL)
 		return (-1);
-	while (l < r)
+	while (l <= r)
 	{
 		mid = (l + r) / 2;
 		printf("Searching in array: ");
