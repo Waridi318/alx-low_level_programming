0b6a1247a4946d7ddab567f34b323067e5c50093
Waridi318 <rosenjeri7151@gmail.com>
2023-07-12 13:24:32 +0300

    Task 7

---
+++ 0x1E-search_algorithms/100-jump.c
@@ -0,0 +1,52 @@
+#include "search_algos.h"
+/**
+* jump_search - jump search algorithm
+* @array: ptr->array[0]
+* @size: size of array
+* @value: value to search for
+* Return: index of value or -1 if not found
+*/
+
+int jump_search(int *array, size_t size, int value)
+{
+	size_t i, k, l, jumps;
+
+	if (array != NULL)
+	{
+		jumps = sqrt(size);
+		for (i = 0; i < (size + jumps); i += jumps)
+		{
+			if (array[i] >= value && i < size)
+			{
+				if (i == 0 && value < array[0])
+				{
+					printf("Value checked array[0] = [%d]\n", array[0]);
+					return (-1);
+				}
+				printf("Value found between indexes [%lu] and [%lu]\n", (i - jumps), i);
+				for (k = (i - jumps); k <= i; k++)
+				{
+					printf("Value checked array[%lu] = [%d]\n", k, array[k]);
+					if (array[k] == value)
+						return (k);
+				}
+			}
+			else if (i >= size)
+			{
+				printf("Value found between indexes [%lu] and [%lu]\n", (i - jumps), i);
+				for (l = (i - jumps); l < size; l++)
+				{
+					printf("Value checked array[%lu] = [%d]\n", l, array[l]);
+					if (array[l] == value)
+						return (l);
+				}
+			}
+			else
+			{
+				printf("Value checked array[%lu] = [%d]\n", i, array[i]);
+				continue;
+			}
+		}
+	}
+	return (-1);
+}
