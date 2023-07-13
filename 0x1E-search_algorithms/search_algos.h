0b9da24d2b741c5eacd84366e4bef45d50c2ec43
Waridi318 <rosenjeri7151@gmail.com>
2023-07-13 05:53:42 +0300

    Added binary_searcha function prototype to header file

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -38,10 +38,12 @@ typedef struct listint_s
 
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
+int binary_searcha(int *array, size_t low, size_t high, int value);
 int advanced_binary(int *array, size_t size, int value);
 int jump_search(int *array, size_t size, int value);
 int interpolation_search(int *array, size_t size, int value);
 int exponential_search(int *array, size_t size, int value);
+int *create_array(int *array, size_t size, size_t start_index);
 listint_t *jump_list(listint_t *list, size_t size, int value);
 skiplist_t *linear_skip(skiplist_t *list, int value);
 #endif
